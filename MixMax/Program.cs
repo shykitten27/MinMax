1dcdb0788c263996b5f313721ddc6744e08ae147
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-12-01 15:21:38 -0600

    init commit

---
+++ MixMax/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace MixMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string of numbers separated by commas and hit enter when finished.");
+            string input = Console.ReadLine();
+            //parse into array based on commas
+            int[] ArrOfNums= Array.ConvertAll<string, int>(input.Split(','), Convert.ToInt32);
+            //sort array
+            Array.Sort(ArrOfNums);
+            //establish min and max
+            int min = ArrOfNums[0];
+            int max = ArrOfNums[ArrOfNums.Length - 1];
+
+            //format output
+            Console.Write("MixMax ([");
+            //for (int i=0; i > ArrOfNums.Length-1; i++)
+            foreach(int i in ArrOfNums)
+            {
+                Console.Write(i);
+                Console.Write(",");
+            }
+            Console.Write(")] -> [");
+            Console.Write(min);
+            Console.Write(",");
+            Console.Write(max);
+            Console.Write("]");
+
+            //sort array
+            //set min and max
+            //print output
+            //end
+        }
+    }
+}
